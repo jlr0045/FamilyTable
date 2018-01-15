f037461d6be0b4def13a288ab66f5b95b8fb2043
Jesse Roach <jroach12345@gmail.com>
2018-01-15 00:11:48 -0600

    added dropdown show/hide

--- simpleFunctions.js
+++ simpleFunctions.js
@@ -1,3 +1,24 @@
-function myFunction() {
-    document.getElementById("demo").innerHTML = "Hello World";
+function serveTheInfo() {
+    document.getElementById("listOfAvailablePeople").innerHTML = "Fill me in with names";
+}
+
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function dayDropBtn() {
+    document.getElementById("dayDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
 }
