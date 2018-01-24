aa1a995033059aff18fa80dbcc593e7179fe687e
jlr0045 <jroach12345@gmail.com>
2018-01-23 22:05:27 -0600

    Filled out time functions in js and time button html

--- simpleFunctions.js
+++ simpleFunctions.js
@@ -5,43 +5,85 @@ function showAvailability() {
 function dayButtonPressed(dayIn) {
     switch (dayIn) {
         case "m":
-            document.getElementByID().innerHTML = "";
+            document.getElementByID(container).innerHTML = "";
             break;
         case "t":
-            document.getElementByID().innerHTML = "";
+            document.getElementByID(container).innerHTML = "";
             break;
         case "w":
-            document.getElementByID().innerHTML = "";
+            document.getElementByID(container).innerHTML = "";
             break;
         case "r":
-            document.getElementByID().innerHTML = "";
+            document.getElementByID(container).innerHTML = "";
             break;
         case "f":
-            document.getElementByID().innerHTML = "";
+            document.getElementByID(container).innerHTML = "";
             break;
     }
     //document.getElementById("dayDropdown").classList.toggle("show");
 }
 
-function timeButtonPressed() {
-    document.getElementById("dayDropdown").classList.toggle("show");
+function timeButtonPressedMWF(timeIn) {
+  switch (timeIn) {
+      case "8":
+          document.getElementByID(container).innerHTML = "";
+          break;
+      case "9":
+          document.getElementByID(container).innerHTML = "";
+          break;
+      case "10":
+          document.getElementByID(container).innerHTML = "";
+          break;
+      case "11":
+          document.getElementByID(container).innerHTML = "";
+          break;
+      case "12":
+          document.getElementByID(container).innerHTML = "";
+          break;
+      case "13":
+          document.getElementByID(container).innerHTML = "";
+          break;
+      case "14":
+          document.getElementByID(container).innerHTML = "";
+          break;
+      case "15":
+          document.getElementByID(container).innerHTML = "";
+          break;
+      case "16":
+          document.getElementByID(container).innerHTML = "";
+          break;
+      case "17":
+          document.getElementByID(container).innerHTML = "";
+          break;
+  }
 }
 
 function showTableAvailabilityButtonPressed() {
     document.getElementById("dayDropdown").classList.toggle("show");
 }
 
-// Close the dropdown if the user clicks outside of it
-//window.onclick = function(event) {
-//  if (!event.target.matches('.dropbtn')) {
-//
-//    var dropdowns = document.getElementsByClassName("dropdown-content");
-//    var i;
-//    for (i = 0; i < dropdowns.length; i++) {
-//      var openDropdown = dropdowns[i];
-//      if (openDropdown.classList.contains('show')) {
-//        openDropdown.classList.remove('show');
-//      }
-//    }
-//  }
-//}
+function timeButtonPressedTR(timeIn) {
+  switch (timeIn) {
+      case "8":
+          document.getElementByID(container).innerHTML = "";
+          break;
+      case "930":
+          document.getElementByID(container).innerHTML = "";
+          break;
+      case "11":
+          document.getElementByID(container).innerHTML = "";
+          break;
+      case "1230":
+          document.getElementByID(container).innerHTML = "";
+          break;
+      case "14":
+          document.getElementByID(container).innerHTML = "";
+          break;
+      case "1530":
+          document.getElementByID(container).innerHTML = "";
+          break;
+      case "17":
+          document.getElementByID(container).innerHTML = "";
+          break;
+  }
+}
